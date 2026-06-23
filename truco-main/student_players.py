14a91caf1d1a50957b0dd953599a0bc809a2d434
Vinicius-Brasil <vini2007brasil@gmail.com>
2026-06-23 15:48:47 -0300

    1o commit: nome e ra para NonePlayer

--- truco-main/student_players.py
+++ truco-main/student_players.py
@@ -1,8 +1,8 @@
 ### TODO: PREENCHA SUAS INFORMAÇÕES AQUI ###
 # Nome #01 (quem entregou o código):    [NOME COMPLETO #01] 
 # RA #01 (quem entregou o código):      [RA #01]
-# Nome #02:                             [NOME COMPLETO #02]
-# RA #02:                               [RA #02]
+# Nome #02:                             Vinicius Brasil Turibio da Silva
+# RA #02:                               306565
 from basic_players import Player
 
 # Implemente neste arquivo seus jogadores para Truco
@@ -10,8 +10,8 @@ from basic_players import Player
 # Jogador que não faz nada. Substitua esta classe para criar as suas, devem herdar da classe Player
 class NonePlayer(Player):
     # Se estiver dúvida sobre como começar olhe os players prontos em basic_players.py e o ReadMe
-    def __init__(self):
-        super().__init__(0, "Ninguém") # Nome do Jogador
+    def __init__(self, ra, name):
+        super().__init__(ra, name) # Nome do Jogador
 
     # Função para retornar o que você vai jogar em determinada mão
     def play(self, top_card, play_hist, score_hist):
@@ -26,9 +26,11 @@ class NonePlayer(Player):
 
 # Função que define o nome da dupla:
 def pair_name():
-    return "algum nome"  # Defina aqui o nome da sua dupla
+    return "Butequeiros de CC"  # Defina aqui o nome da sua dupla
 
 
 # Função que cria a dupla:
 def create_pair():
-    return (NonePlayer(), NonePlayer())  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
+    p1 = NonePlayer(11, "Stalin")
+    p2 = NonePlayer(12, "Lenin")
+    return (p1,p2)  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
