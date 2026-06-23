86f9c578ebe45e5cb2a2b180ce80d0e71f89c943
andre <dealmeida.andre08@gmail.com>
2026-06-23 11:51:28 -0300

    adicionando nome e ra para o NonePlayer

--- truco-main/student_players.py
+++ truco-main/student_players.py
@@ -6,29 +6,36 @@
 from basic_players import Player
 
 # Implemente neste arquivo seus jogadores para Truco
+DECISAO = {"encoberta": 0, "normal": 1, "truco": 2}
+RESPOSTA = {"correr": 0, "aceitar": 1, "aumentar": 2}
 
 # Jogador que não faz nada. Substitua esta classe para criar as suas, devem herdar da classe Player
 class NonePlayer(Player):
     # Se estiver dúvida sobre como começar olhe os players prontos em basic_players.py e o ReadMe
-    def __init__(self):
-        super().__init__(0, "Ninguém") # Nome do Jogador
+    def __init__(self, ra, name):
+        super().__init__(ra, name) # Nome do Jogador
 
+    """O JOGO ESTA DEFINIDO AQUI"""
     # Função para retornar o que você vai jogar em determinada mão
     def play(self, top_card, play_hist, score_hist):
         if self._cards:
-            return 1, self._cards[0]
+            # retorna (jogar carta, primeira carta)
+            return DECISAO["normal"], self._cards[0]
         return 1, None
 
     # Função para retornar o que você vai dar de resposta a trucos
     def respond(self,top_card,play_hist, score_hist):
-        return 0
+        return RESPOSTA["correr"]
 
 
 # Função que define o nome da dupla:
 def pair_name():
-    return "algum nome"  # Defina aqui o nome da sua dupla
+    return "Butequeiros de CC"  # Defina aqui o nome da sua dupla
 
 
 # Função que cria a dupla:
 def create_pair():
-    return (NonePlayer(), NonePlayer())  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
+    p1 = NonePlayer(11, "Stalin")
+    p2 = NonePlayer(12, "Lenin")
+    print(p1, p2)
+    return (p1, p2)  # Defina aqui a dupla de jogadores. Deve ser uma tupla com dois jogadores.
